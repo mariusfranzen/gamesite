e05fffaa8679a111376c8417ce0846e4ac4ace3e
Marius Franzén <marius.franzen1@gmail.com>
2020-06-24 11:48:45 +0200

    Added skeleton

---
+++ src/components/app.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import { Router, Route, Switch } from "react-router-dom";
+
+// styles
+import "../sass/main.scss";
+// pages
+import HomePage from "../pages/Home";
+// components
+import Navigation from "./common/navigation";
+import history from "./common/history";
+
+export default class componentName extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    };
+    render() {
+        return (
+            <React.Fragment>
+                <Router history={history}>
+                    <Navigation />
+                    <Switch>
+                        <Route
+                            exact
+                            path="/"
+                            render={(props) => (
+                                <HomePage {...props} />
+                            )}
+                        />
+                    </Switch>
+                </Router>
+            </React.Fragment>
+        );
+    }
+}
